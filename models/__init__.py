a85e18e996472c405467647874762aab7b5f915c
yuanruntian <47597768+yuanruntian@users.noreply.github.com>
2024-09-26 15:23:21 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,5 @@ from .onlinerefer import build as build_online
 def build_model(args):
     if args.online:
         return build_online(args)
-    elif args.semi_online:
-        raise Exception(f'Now we does not support semi-online mode.')
     else:
         return build(args)
