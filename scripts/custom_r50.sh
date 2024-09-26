9264d18bb473ff92d4b798d1cec90350299c3e7b
yuanruntian <47597768+yuanruntian@users.noreply.github.com>
2024-09-26 15:27:26 +0800

    Create custom_r50.sh

---
+++ scripts/custom_r50.sh
@@ -0,0 +1,30 @@
+OUTPUT_DIR=/work_dir/custom_r50
+
+# training
+python3 -m torch.distributed.launch --nproc_per_node=2 --master_port=29000 --use_env \
+main.py --with_box_refine --freeze_text_encoder --binary \
+--lr_drop 3 5 \
+--lr=1e-4 \
+--lr_backbone=5e-6 \
+--num_frames=3 \
+--sampler_steps 4 \
+--sampler_lengths 2 3 \
+--sampler_interval=5 \
+--output_dir=${OUTPUT_DIR} \
+--online \
+--epochs 5 \
+--pretrained_weights=/work_dir/pretrained_weights/r50_pretrained.pth 
+
+
+# inference
+CHECKPOINT=${OUTPUT_DIR}/checkpoint.pth
+python3 inference_custom.py --with_box_refine --freeze_text_encoder --binary \
+--output_dir=${OUTPUT_DIR} --resume=${CHECKPOINT} \
+--ngpu=2 \
+--online \
+
+python eval_custom.py --path ${OUTPUT_DIR}
+
+echo "Working path is: ${OUTPUT_DIR}"
+
+# CUDA_VISIBLE_DEVICES=0,1 sh scripts/custom_r50.sh 
